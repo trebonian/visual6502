d94116d2579ad1c7d4915f253fc34a4f9057793a
Barry Silverman <barry@ubuntu-lynx.(none)>
2010-09-27 20:56:47 -0400

    Fixed save.php

--- save.php
+++ save.php
@@ -1,9 +1,9 @@
 <?php
 
 $filename = $_REQUEST['name'];
-file_put_contents($filename, file_get_contents("php://input"));
+file_put_contents1($filename, file_get_contents("php://input"));
 
-function file_put_contents($filename, $data) {
+function file_put_contents1($filename, $data) {
 	$f = @fopen($filename, 'w');
 	if ($f) {
 		$bytes = fwrite($f, $data);
