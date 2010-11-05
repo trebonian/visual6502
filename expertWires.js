93e6d662dfc953cebdbdd11dcc886d9441054c10
BigEd <ed.spittles@gmail.com>
2010-11-05 18:25:51 +0000

    panes resize when chip graphics are shown/hidden

--- expertWires.js
+++ expertWires.js
@@ -317,24 +317,29 @@ var chipsurround;
 function updateChipLayoutVisibility(isOn){
 	chipLayoutIsVisible=isOn;
 	if(chipLayoutIsVisible) {
+		updateChipLayoutAnimation(true);
+		// resize the two panes appropriately
+		$("#frame").trigger("resize", [ 810 ]);
+		$("#rightcolumn").trigger("resize", [ 738 - 180 ]);
+		// replace the Show Chip button with the chip graphics
 		chipsurround=document.getElementById('chipsurround');
 		chipsurround.style.display = 'block';
 		if(expertMode)
 			document.getElementById('layoutControlPanel').style.display = 'block';
 		document.getElementById('nochip').style.display = 'none';
-		//document.getElementById('logstreamscroller').style.height="260px";
-		updateChipLayoutAnimation(true);
-		setStatus('loading graphics...');
 		// allow the browser to respond while we load the graphics
+		setStatus('loading graphics...');
 		setTimeout(setupChipLayoutGraphics, 0);
 	} else {
 		// cannot animate the layout if there is no canvas
 		updateChipLayoutAnimation(false);
+		// resize the two panes appropriately
+		$("#frame").trigger("resize", [ 120 ]);
+		$("#rightcolumn").trigger("resize", [ 200 ]);
 		// replace the layout display with a button to show it
 		document.getElementById('chipsurround').style.display = 'none';
 		document.getElementById('layoutControlPanel').style.display = 'none';
 		document.getElementById('nochip').style.display = 'block';
-		//document.getElementById('logstreamscroller').style.height="880px";
 	}
 }
 
