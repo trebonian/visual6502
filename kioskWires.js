b3e58dcbabca15a767bea11a0a9862c1b5429357
BigEd <ed.spittles@gmail.com>
2010-11-22 15:20:24 +0000

    immunize kiosk mode against I/O triggers in test program

--- kioskWires.js
+++ kioskWires.js
@@ -88,6 +88,7 @@ function setup_part2(){
 
 function setup_part3(){
 	loadProgram();
+	writeTriggers={};  // kiosk mode does not handle I/O
 	initChip();
 	document.getElementById('stop').style.visibility = 'hidden';
 	go();
