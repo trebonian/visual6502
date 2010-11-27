172394845adfdd0b9d723cb79b2c6c76e0124c69
BigEd <ed.spittles@gmail.com>
2010-11-27 14:30:19 +0000

    [bug]typo: fixed B bit of status register

--- macros.js
+++ macros.js
@@ -262,7 +262,7 @@ function readPstring(){
    result = (isNodeHigh(nodenames['p7'])?'N':'n') +
             (isNodeHigh(nodenames['p6'])?'V':'v') +
             '&#8209' +  // non-breaking hyphen
-            (isNodeHigh(nodenames['p3'])?'B':'b') +
+            (isNodeHigh(nodenames['p4'])?'B':'b') +
             (isNodeHigh(nodenames['p3'])?'D':'d') +
             (isNodeHigh(nodenames['p2'])?'I':'i') +
             (isNodeHigh(nodenames['p1'])?'Z':'z') +
