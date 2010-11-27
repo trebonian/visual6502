13e1f51b47fdb61f570cbd8a381e3be9cc1ab550
BigEd <ed.spittles@gmail.com>
2010-11-27 13:54:31 +0000

    kiosk mode: add link to user guide, tweak help text and add alphabetic zoom controls

--- kioskWires.js
+++ kioskWires.js
@@ -104,9 +104,9 @@ function setup_part3(){
 function handleKey(e){
 	var c = e.charCode;
 	c = String.fromCharCode(c);
-	if('<>?np'.indexOf(c)==-1) return;
-	if(c=='<' && zoom>1) setZoom(zoom/1.2);
-	else if(c=='>' && zoom<grMaxZoom) setZoom(zoom*1.2);
+	if('zx<>?np'.indexOf(c)==-1) return;
+	if((c=='x' || c=='<') && zoom>1) setZoom(zoom/1.2);
+	else if((c=='z' || c=='>') && zoom<grMaxZoom) setZoom(zoom*1.2);
 	else if(c=='?') setZoom(1);
 	else if(c=='n') stepForward();
 	else if(c=='p') stepBack();
