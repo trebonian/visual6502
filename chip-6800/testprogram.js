c6dd03ba17dcc171557c6ad3ce2842edaaf5f40b
BigEd <ed.spittles@gmail.com>
2011-04-05 18:09:53 +0000

    fixup 6800 test program

--- chip-6800/testprogram.js
+++ chip-6800/testprogram.js
@@ -7,19 +7,19 @@ var consoleboxStream="";
 // for opcodes, see ftp://ftp.comlab.ox.ac.uk/pub/Cards/txt/6800.txt
 
 testprogram = [
-  0x01,              // NOP
-  0x01,              // NOP
-  0x01,              // NOP
   0xce, 0x43, 0x21,  // LDX #4321
   0x35,              // TXS
-  0xc6, 0x00,        // LDAA #$00
+  0xc6, 0xfb,        // LDAB #$FB
   0xbd, 0x00, 0x10,  // JSR $0010
-  0x7e, 0x00, 0x02,  // JMP $0002
+  0x7e, 0x00, 0x04,  // JMP $0004
+  0x01,              // NOP
+  0x01,              // NOP
+  0x01,              // NOP
   0x01,              // NOP
   0x08,              // INX
-  0x5a,              // DECB
+  0x4a,              // DECA
   0x7c, 0x00, 0x0f,  // INC $0F
   0x0d,              // SEC
-  0xc9, 0x02,        // ADCA #$02
+  0xc9, 0x02,        // ADCB #$02
   0x39,              // RTS
 ]
