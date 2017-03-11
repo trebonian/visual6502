51d6bb216bd26e3d42c6efb4cd8d5d8860d55488
mmfoerster <mmfoerster@msn.com>
2017-03-10 23:09:20 -0500

    Patch allNodes() to generate list of numbers for node indexes instead of numeric strings, fix JMP indirect mnemonic
    Patches for the general chipsim code and the 6502 emulation.
    
        For the general chipsim code, allNodes() was constructing a list of
    numeric strings for the node indexes instead of a list of numbers for them.
    During the first iteration inside recalcNodeList(), the numeric string node
    indexes would end up being the first elements of their respective node group
    lists. This in turn would allow instances of a duplicate node index to be added
    to the node group list. The duplicate in each case would be the numeric
    equivalent of the initial numeric string index: indexOf() would not recognize
    the string element as being the equivalent of the numeric node index, so the
    numeric version would be added also.
    
    For an example (from real log data), a node group list of: [ "49", 483 ] has a
    node index of 49 tested against it. indexOf() says "49" is not an occurrence
    of 49, so it allows 49 to be

--- chipsim.js
+++ chipsim.js
@@ -143,7 +143,15 @@ function saveString(name, str){
 
 function allNodes(){
 	var res = new Array();
-	for(var i in nodes) if((i!=npwr)&&(i!=ngnd)) res.push(i);
+	var ii = 0;
+	for(var i in nodes) {
+		// Don't feed numeric strings to recalcNodeList(). Numeric
+		// strings can cause a (data dependent) duplicate node number
+		// hiccup when accumulating a node group's list, ie:
+		// group => [ "49", 483, 49 ]
+		ii = Number( i );
+		if((ii!=npwr)&&(ii!=ngnd)) res.push(ii);
+	}
 	return res;
 }
 
